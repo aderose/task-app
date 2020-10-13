afff8528fb820c437de80c8ea6d7dc0617c1f226
aderose <sandro.derose28@yahoo.com>
2020-10-13 16:02:52 +0100

    Update date into datetime. Implement form cancel button

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -2,24 +2,28 @@ import pubsub from "./pubsub";
 
 // handle form input provided by the user
 const inputHandler = (doc) => {
-  doc.querySelector(".add").addEventListener("click", () => {
-    doc.querySelector(".form-container").classList.toggle("visible");
-  });
+  doc.querySelector(".add").addEventListener("click", toggleForm);
 
   const form = doc.querySelector("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     gatherInput(form);
-    doc.querySelector(".form-container").classList.toggle("visible");
+    toggleForm();
   });
+
+  doc.querySelector("#cancel").addEventListener("click", toggleForm);
 
   function gatherInput(form) {
     const formData = new FormData(form);
     pubsub.publish("addTask", {
       title: formData.get("title"),
-      date: formData.get("date"),
+      date: formData.get("datetime-local"),
       priority: formData.get("priority"),
     });
+  }
+
+  function toggleForm() {
+    doc.querySelector(".form-container").classList.toggle("visible");
   }
 };
 
