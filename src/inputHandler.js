3df51bdf1225078f7b0c0d6181f5f6907fce2254
aderose <sandro.derose28@yahoo.com>
2020-10-22 23:04:04 +0100

    Abstract event listener creation to allow for easier listener removal (old listeners still present)

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -2,6 +2,8 @@ import pubsub from "./pubsub";
 
 // handle all document event listeners
 const inputHandler = () => {
+  const activeListeners = [];
+
   // subscribe to all create Listener events to listen for user input
   pubsub.subscribe("createEditListener", editListener);
   pubsub.subscribe("createDeleteListener", trashListener);
@@ -14,11 +16,35 @@ const inputHandler = () => {
   pubsub.subscribe("createMenuListener", menuListener);
 
   pubsub.subscribe("createEventListener", createEventListener);
+  pubsub.subscribe("deleteEventListener", deleteEventListener);
 
   // create an event listener for the provided properties
-  function createEventListener(properties) {
-    properties.element.addEventListener(properties.type, properties.fn);
+  function createEventListener({ element, type, fn }) {
+    activeListeners.push({ element, type, fn });
+    element.addEventListener(type, fn);
   }
+
+  // remove an event listener with the provided properties
+  function deleteEventListener({ element, type }) {
+    // get index of listener in the active listeners list
+    const index = activeListeners.findIndex((listener) => {
+      return listener.element === element && listener.type === type;
+    });
+
+    // if no listener is found
+    if (index === -1) {
+      console.log("deleteEventListener: no listener found!");
+      return -1;
+    }
+
+    // remove listener from active listeners
+    const listener = activeListeners.splice(index, 1)[0];
+
+    // remove event listener from the element.
+    listener.element.removeEventListener(listener.type, listener.fn);
+  }
+
+  // ---------------- OLD -------------------------
 
   // publish an "addForm" event when the add task button is clicked
   function addListener(add) {
